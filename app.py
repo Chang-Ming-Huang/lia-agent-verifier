2bd2b1f8bb3b011221defad2b199de605fb2c12a
Chang-Ming-Huang <jmhuang8829@gmail.com>
2025-11-28 00:22:35 +0800

    Add env var support

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Render! 這是自動佈署的成果！"
+    # 嘗試讀取名為 'MY_NAME' 的環境變數，如果讀不到，預設值就是 'Guest'
+    user_name = os.environ.get('MY_NAME', 'Guest')
+    return f"Hello, {user_name}! 這是一個使用環境變數的測試。"
 
 if __name__ == '__main__':
     app.run(debug=True)
